82b85f18a2a0cbf3e0660031179742c8a075d8b6
Wok <woctezuma@users.noreply.github.com>
2020-08-07 21:18:30 +0200

    Dummy unit test

---
+++ test_.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestDummyMethods(unittest.TestCase):
+
+    def test_dummy_method(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
+
